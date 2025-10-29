6591c479db94ca5152d109abfe0c8f37edad3e2b
victorpinheiro2103-spec <victorpinheiro2103@gmail.com>
2025-10-29 19:51:07 -0300

    Add files via upload

--- trabalho/src/php/config.php
+++ trabalho/src/php/config.php
@@ -16,3 +16,37 @@ else
 }
 ?>
 
+<?php 
+if(isset($_POST['submit']))
+{
+    print_r('Nome Completo: ' . $_POST['nome']);
+    print_r("<br>");
+    print_r('Email: ' . $_POST['email']);
+    print_r("<br>");
+    print_r('Telefone: ' . $_POST['telefone']);
+    print_r("<br>");
+    print_r('Gênero: ' . $_POST['genero']);
+    print_r("<br>");
+    print_r('Data de Nascimento: ' . $_POST['data']);
+    print_r("<br>");
+    print_r('Endereço: ' . $_POST['endereco']);
+    print_r("<br>");
+    print_r('Cidade: ' . $_POST['cidade']);
+    print_r("<br>");
+    print_r('Estado: ' . $_POST['estado']);
+
+    include_once('config.php');
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $genero = $_POST['genero'];
+    $data = $_POST['data']; 
+    $endereco = $_POST['endereco'];
+    $cidade = $_POST['cidade'];
+    $estado = $_POST['estado'];
+
+    $result = mysqli_query($conexao, "INSERT INTO clientes(nome, email, telefone, genero, data_nascimento, endereco, cidade, estado) 
+        VALUES ('$nome', '$email', '$telefone', '$genero', '$data', '$endereco', '$cidade', '$estado')");
+}
+?>
