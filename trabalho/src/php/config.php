bf1980d71acff2203597a05b077c5dd1e4384f1c
Cauã Souza <158606087+CaUA2401@users.noreply.github.com>
2025-11-12 13:23:44 -0300

    Add files via upload

--- trabalho/src/php/config.php
+++ trabalho/src/php/config.php
@@ -1,22 +1,5 @@
 <?php 
-$dbHost = 'guest@localhost';
-$dbUsername = 'root';
-$dbPassword = 'CSdAC@2006';
-$dbName = 'bank';
 
-$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-
-if($conexao->connect_error)
-{
-    echo "Erro";
-}
-else
-{
-    echo "Conectado com sucesso!";
-}
-?>
-
-<?php 
 if(isset($_POST['submit']))
 {
     print_r('Nome Completo: ' . $_POST['nome']);
@@ -27,13 +10,16 @@ if(isset($_POST['submit']))
     print_r("<br>");
     print_r('Gênero: ' . $_POST['genero']);
     print_r("<br>");
-    print_r('Data de Nascimento: ' . $_POST['data']);
+    print_r('Data de Nascimento: ' . $_POST['data_nasc']);
     print_r("<br>");
     print_r('Endereço: ' . $_POST['endereco']);
     print_r("<br>");
     print_r('Cidade: ' . $_POST['cidade']);
     print_r("<br>");
     print_r('Estado: ' . $_POST['estado']);
+    print_r("<br>");
+    print_r('Senha: ' . $_POST['senha']);
+
 
     include_once('config.php');
 
@@ -47,6 +33,6 @@ if(isset($_POST['submit']))
     $estado = $_POST['estado'];
 
     $result = mysqli_query($conexao, "INSERT INTO clientes(nome, email, telefone, genero, data_nascimento, endereco, cidade, estado) 
-        VALUES ('$nome', '$email', '$telefone', '$genero', '$data', '$endereco', '$cidade', '$estado')");
+        VALUES ('$nome', '$email', '$telefone', '$genero', '$data_nasc', '$endereco', '$cidade', '$estado')");
 }
 ?>
